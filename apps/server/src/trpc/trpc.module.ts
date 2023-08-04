1c4f321c50b494ceb0b72544944ced15e6caffd0
cieko <rahulsaxena2021@hotmail.com>
2023-08-04 15:04:28 +0530

    trpc server

--- apps/server/src/trpc/trpc.module.ts
+++ apps/server/src/trpc/trpc.module.ts
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TrpcService } from './trpc.service';
+import { TrpcRouter } from './trpc.router';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  providers: [TrpcService, TrpcRouter],
 })
 export class TrpcModule {}
